00f0fca036e4f32dc68c2e3d79e68cff439feca9
SOHick <shnikitaprog@mail.ru>
2022-11-11 01:28:35 +0300

    Initial commit

---
+++ src/main/java/ru/SOHick/App.java
@@ -0,0 +1,13 @@
+package ru.SOHick;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/ru/SOHick/math/Complex.java
@@ -0,0 +1,2 @@
+package ru.SOHick.math;public class Complex {
+}
